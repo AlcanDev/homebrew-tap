7c5ffd10c8671b6139617909858e31e9c53d7e0e
Felipe Alcántara <51348034+AlcanDev@users.noreply.github.com>
2026-05-04 22:35:51 -0400

    chore: update korva formula to v1.1.0

--- Formula/korva.rb
+++ Formula/korva.rb
@@ -4,27 +4,27 @@ class Korva < Formula
   desc "AI ecosystem CLI for enterprise development teams"
   homepage "https://korva.dev"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/AlcanDev/korva/releases/download/v1.0.0/korva_1.0.0_darwin_arm64.tar.gz"
-      sha256 "fb2731ab41183d25d080ceb8289fe5cb0e86677a6af3e5a6cecb98fe0f4329cf"
+      url "https://github.com/AlcanDev/korva/releases/download/v1.1.0/korva_1.1.0_darwin_arm64.tar.gz"
+      sha256 "1bd39db9dbf1efdc0a076df44e66ed962130daf859571ad08efb711d9b5a87e2"
     end
     on_intel do
-      url "https://github.com/AlcanDev/korva/releases/download/v1.0.0/korva_1.0.0_darwin_amd64.tar.gz"
-      sha256 "a5cadc49da4117091c7a6ebe23b6ccc896b8029e8ea340336774d7c1fdb3e36e"
+      url "https://github.com/AlcanDev/korva/releases/download/v1.1.0/korva_1.1.0_darwin_amd64.tar.gz"
+      sha256 "5a4e016cba628607c87ac43c4c80fee1e974b8cfe40a52d08b698adf3cb50ffa"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/AlcanDev/korva/releases/download/v1.0.0/korva_1.0.0_linux_arm64.tar.gz"
-      sha256 "b02f4a101b0f1602e8c4b610793ea99fe529e170c8bc790633c6aa2650f866c4"
+      url "https://github.com/AlcanDev/korva/releases/download/v1.1.0/korva_1.1.0_linux_arm64.tar.gz"
+      sha256 "ae0641b6c5bbefc0d1083cd185806d0eac793967526e90ab760c955d37743759"
     end
     on_intel do
-      url "https://github.com/AlcanDev/korva/releases/download/v1.0.0/korva_1.0.0_linux_amd64.tar.gz"
-      sha256 "10e22e9149b3bf9151f4b7f96c2587a4d9d2164b971c14b3707634d11d5d4e77"
+      url "https://github.com/AlcanDev/korva/releases/download/v1.1.0/korva_1.1.0_linux_amd64.tar.gz"
+      sha256 "00153e3bcd3f2c5a6e799603abfc957662e445a6a8b15595589f21ed1b4ec1ff"
     end
   end
 
